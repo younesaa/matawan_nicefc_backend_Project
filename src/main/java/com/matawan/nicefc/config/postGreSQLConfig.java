2113c678229425188d0901f6dddac9e775e09414
younes_lak <younes.lakhnichy@capgemini.com>
2024-01-13 02:06:38 +0100

    first commit

---
+++ src/main/java/com/matawan/nicefc/config/postGreSQLConfig.java
@@ -0,0 +1,21 @@
+package com.matawan.nicefc.config;
+
+
+import com.opentable.db.postgres.embedded.EmbeddedPostgres;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class postGreSQLConfig {
+    @Bean
+    @Primary
+    public DataSource embeddedPostGreSqlDb() throws Exception {
+        DataSource embeddedPostgresDS = EmbeddedPostgres.builder()
+                .start().getPostgresDatabase();
+
+        return embeddedPostgresDS;
+    }
+}
---
+++ src/test/java/com/matawan/nicefc/NicefcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matawan.nicefc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicefcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
